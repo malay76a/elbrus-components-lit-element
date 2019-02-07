9d85b9b3f00d31f233df765a301fa8d2eece9f41
Valentin Fedyakov <dagot32167@gmail.com>
2019-02-07 19:25:04 +0300

    split building

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,13 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import postcss from 'rollup-plugin-postcss'
 
-export default {
+export default [{
     input: ['src/components/index.js'],
-    output: [{
+    output: {
         file: 'build/module/components/index.js',
         format: 'es',
         sourcemap: true
-    },{
-        file: 'build/nomodule/components/index.js',
-        format: 'system',
-        sourcemap: true
-    }],
+    },
     plugins: [
         resolve(),
         babel(),
@@ -20,5 +16,20 @@ export default {
             extract: false,
             plugins: []
         })
-    ]
-};
+    ]},
+    {
+        input: ['src/components/index.js'],
+        output: {
+            file: 'build/nomodule/components/index.js',
+            format: 'system',
+            sourcemap: true
+        },
+        plugins: [
+            resolve(),
+            babel(),
+            postcss({
+                extract: false,
+                plugins: []
+            })
+        ]
+    }];
